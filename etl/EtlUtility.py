8eb1aa55958de537ae1d3ed6884e810947cd669f
qiutiaoming <qtm315@qq.com>
2013-09-17 11:32:34 +0800

    添加getweek(返回给定时间在一年中的那一周)

--- etl/EtlUtility.py
+++ etl/EtlUtility.py
@@ -11,10 +11,12 @@ def SqlExecCode(etl_op,exec_namespace):
     exec("subdate = lambda x,y:time.strftime('%Y%m%d',time.gmtime(int(time.mktime(time.strptime(x,'%Y%m%d'))) - y*24*3600 + 8*3600))") in exec_namespace
     exec("getyear = lambda x:x[:4]") in exec_namespace
     exec("getmonth = lambda x:x[4:6]") in exec_namespace
+    exec("getweek = lambda x:time.strftime('%W',time.strptime(x,'%Y%m%d'))") in exec_namespace
     exec("lastmonth = lambda x:time.strftime('%m',time.gmtime(int(time.mktime(time.strptime(x,'%Y%m%d'))) - int(x[6:8])*24*3600 + 8*3600))")  in exec_namespace
     exec("lastmonthday = lambda x,y:time.strftime('%Y%m',time.gmtime(int(time.mktime(time.strptime(x,'%Y%m%d'))) - int(x[6:8])*24*3600 + 8*3600)) + '%02d' % y")  in exec_namespace
     exec("curweek = lambda x,y:subdate(x,calendar.weekday(int(x[:4]),int(x[4:6]),int(x[6:])) - y)") in exec_namespace
     exec("lastweek = lambda x,y:subdate(x,calendar.weekday(int(x[:4]),int(x[4:6]),int(x[6:])) - y + 7)") in exec_namespace
+    exec("theweek = lambda x,y:subdate(x,calendar.weekday(int(x[:4]),int(x[4:6]),int(x[6:])) - y + 7)") in exec_namespace
     exec("addhour = lambda x,y:int(x)+y") in exec_namespace
     exec("subhour = lambda x,y:int(x)-y") in exec_namespace
     exec("date = '%s'" % etl_op['date']) in exec_namespace
